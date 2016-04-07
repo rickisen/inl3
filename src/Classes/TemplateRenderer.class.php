49073896645ee62a2271342b883d06fa48547863
rickisen <rickard.spam@gmail.com>
2016-04-07 21:39:57 +0200

    Adds basic routing and support for api and DB

---
+++ src/Classes/TemplateRenderer.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class TemplateRenderer{
+  private function __construct(){}
+  private function __clone(){}
+
+  public static function render($data){
+    include 'Templates/header.php';
+
+    include 'Templates/'.$data['loadview'].'.php';
+
+    include 'Templates/footer.php';
+  }
+}
--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
 <?php
+require_once 'Classes/URL.class.php'; 
 
-echo "Hello";
-
+// if we got an API call, Call the API and 
+// return that data in a json file. 
+// Else create and send a HTML page
+if ( URL::getUrlParts()[0] == 'api' ){
+  echo "api call!";
+} else {
+  // Render the page from php templates
+  $data = ['loadview' => 'main']; // placeholder
+  require_once("Classes/TemplateRenderer.class.php"); 
+  TemplateRenderer::render($data);
+}
