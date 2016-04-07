6af1c8fe9287e38d4d459ce4b506cc69d0a60fcc
rickisen <rickard.spam@gmail.com>
2016-04-07 17:12:03 +0200

    Adds support for vagrant and sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,32 @@
-require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
 
-// Project configuration.
-grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	sass: {
-		options: {
-			sourceMap: true
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'build/Stylesheets/main.css': 'src/Stylesheets/main.scss'
+				}
+			}
 		},
-		dist: {
-			files: {
-				'build/stylesheets/sass/main.css': '/sass/main.scss'
+		copy : {
+			main : {
+				files : [
+					{
+						expand : true,
+						cwd    : 'src',
+						src    : ['index.php', 'Scripts', 'Classes', 'Templates'],
+						dest   : 'build/',
+					}
+				]
 			}
 		}
-	}
-});
+	});
 
-grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['sass', 'copy']);
+};
