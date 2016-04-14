129dd6742bbb008c92f00002207d7b08820d8e51
rickisen <rickard.spam@gmail.com>
2016-04-14 09:54:20 +0200

    Added jquery cdn

--- src/Templates/footer.php
+++ src/Templates/footer.php
@@ -1,6 +1,7 @@
     <footer> 
 
     </footer>
+    <script src="http://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
     <script src="Scripts/concat.js" type="text/javascript" charset="utf-8"></script>
   </body>
 </html>
