6b8f047571eef63b136e8071ed05db8b9c0784bb
rickisen <rickard.spam@gmail.com>
2016-04-16 13:43:12 +0200

    Uses more jquery

--- src/Scripts/populate.js
+++ src/Scripts/populate.js
@@ -1,61 +1,73 @@
 'use strict'
 
 var main = document.getElementsByTagName('main')[0];
+var currentCollection = "";
 
-loadSections();
-loadMunks();
+loadNav();
 
-function loadSections() {
-	loadJson('/?/api/Sections', function(json){
-		let data = JSON.parse(json);
-		let ul = document.getElementsByTagName('header')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0]; 
-		
-		for ( var i = 0, len = data.length; i < len; i++ ) {
-			let li = ul.getElementsByTagName('li')[i]; 
-			if ( ! li ) {
-				li = ul.getElementsByTagName('li')[0].cloneNode(true); 
-				ul.appendChild(li); 
-			}
+function loadNav() {
+	$.getJSON('/?/api/section', populateNav); 
+}
 
-			li.textContent = data[i]['name']; 
-		}
+function loadSection(event) {
+	var li = event.target ;
+	$.getJSON('/?/api/section/' + li.getAttribute('section-id'), function(data){
+		main.getElementsByTagName('article')[0].getElementsByTagName("h2")[0].textContent = data['title']; 
+		loadArticle(data['primaryContent']); 
 	}); 
 }
 
-function loadMunks() {
-	loadJson('/?/api/Munks', function(json){
-		let data = JSON.parse(json);
-		let article = main.getElementsByTagName('article')[0]; 
-		article.getElementsByTagName("h2")[0].textContent = "Munkar"; 
-
-		for ( var i = 0, len = data.length; i < len; i++ ) {
-			let section = article.getElementsByTagName('section')[i]; 
-			if ( ! section ) {
-				section = article.getElementsByTagName('section')[0].cloneNode(true); 
-				article.appendChild(section); 
-			}
-
-			let title = section.getElementsByTagName('h3')[0]; 
-			let paragraf = section.getElementsByTagName('p')[0];
-
-			title.textContent = data[i]['name']; 
-			paragraf.textContent  = data[i]['price']; 
-		}
-	});
+function loadElement(event) {
+	let element = event.currentTarget.getAttribute('element-id'); 
+	$.getJSON('/?/api/' + currentCollection + '/' + element, populateArticle);
 }
 
-function loadJson(query, callback){
-	var http_request = new XMLHttpRequest();
+function loadArticle(collection, element = "" ) {
+	currentCollection = collection; 
+	$.getJSON('/?/api/' + collection + '/' + element, populateArticle);
+}
 
-	http_request = new XMLHttpRequest();
+function populateNav(data){
+	let ul = document.getElementsByTagName('header')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0]; 
 
-	http_request.onreadystatechange = function(){
-		if ( http_request.readyState == 4 && http_request.status == 200 ){
-			callback(http_request.responseText);
+	for ( var i = 0, len = data.length; i < len; i++ ) {
+		let li = ul.getElementsByTagName('li')[i]; 
+		if ( ! li ) {
+			li = ul.getElementsByTagName('li')[0].cloneNode(true); 
+			ul.appendChild(li); 
 		}
+
+		li.textContent = data[i]['title']; 
+		li.setAttribute('section-id', data[i]['id']); 
+		li.addEventListener('click',loadSection); 
+	}
+}
+
+function populateArticle(data){
+	if (!$.isArray(data)) {
+		data = [data]; 
 	}
 
-	http_request.open('GET', query, true);
-	http_request.send();
-}
+	// first clear it of any currently displayed sections
+	var oldSections = $('article').find('section'); 
+	for (var i = 0, len = oldSections.length; i < len; i++) {
+		oldSections[i].remove(); 
+	}
 
+	let article = $('article'); 
+	// loop through the data and populate the article with it.
+	for ( var i = 0, len = data.length; i < len; i++ ) {
+		let newSection = $('<section></section>'); 
+
+		$.getJSON('/?/api/' + currentCollection + '/' + data[i]['id'], function(elementData){
+			newSection.attr('element-id', elementData['id']); 
+			newSection.append('<h3>' + elementData['title'] + '</h3>');
+			newSection.append('<p>' + elementData['description'] + '</p>');
+		}); 
+
+		// adds a listner to the section to load a full articel.
+		newSection.click(loadElement); 
+
+		article.append(newSection); 
+	}
+}
