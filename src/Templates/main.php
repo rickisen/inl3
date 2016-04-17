0ca5a87eea5537f0d789e3f04b37d54c685ddaa8
rickisen <rickard.spam@gmail.com>
2016-04-17 22:49:41 +0200

    retab

--- src/Templates/main.php
+++ src/Templates/main.php
@@ -1,9 +1,9 @@
-    <main role="main">
-      <article>
-          <h2></h2>
-        <section>
-          <h3></h3>
-          <p></p>
-        </section>
-      </article>
-    </main>
+<main role="main">
+<article>
+    <h2></h2>
+    <section>
+        <h3></h3>
+        <p></p>
+    </section>
+</article>
+</main>
